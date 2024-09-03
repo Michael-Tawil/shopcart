897189c438c18453d60c9be9dc82586d65a2373d
Michael-Tawil <michaeltawil3@gmail.com>
2024-09-03 23:28:38 +1000

    header design

--- src/hnav.jsx
+++ src/hnav.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom"
 function Hnav(){
     return(
-        <div className="hnav">
-            <h1>M&A Clothing</h1>
-            <Link to ="/">Home</Link>
-            <Link to ="/Shop">Shop</Link>
-            <Link to ="/Cart">Cart</Link>
+        <div className="hed">
+            <h1>MA Clothing</h1>
+            <div className="hnav">
+                <Link to ="/">Home</Link>
+                <Link to ="/Shop">Shop</Link>
+            </div>
+            <Link to ="/Cart"><img src="/cart.png" className="carticon" alt="Cart" /></Link>
         </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import router from './Routes'
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
-
+import '/src/index.css'
 const routes = createBrowserRouter(router)
 createRoot(document.getElementById('root')).render(
   <StrictMode>
